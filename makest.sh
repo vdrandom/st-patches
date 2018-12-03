143bbcc77eea9609e7c96ecd721d47945bf6cf8b
Von Random <von@vdrandom.org>
2018-12-03 18:23:00 +0300

    initial commit with a set of patches I plan to keep and maintain

---
+++ makest.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env zsh
+ST_REPO=https://git.suckless.org/st
+ST_SRC=$PWD/st
+ST_CFG=$PWD/config.h
+PATCHLIST=(
+    st-boxdraw.patch
+    st-bright-bold-text.patch
+    st-config-mk.patch
+    st-disable-bold-italic-fonts.patch
+    st-disable-intensity-styles.patch
+    st-fix-argv.patch
+)
+
+if [[ ! -r $ST_SRC/.git/config ]]; then
+    git clone $ST_REPO $ST_SRC
+    cd $ST_SRC
+else
+    cd $ST_SRC
+    git reset --hard
+    git clean -fd
+    git pull
+fi
+
+cp $ST_CFG .
+
+for patch in $PATCHLIST; do
+    patch -p1 -i ../$patch
+done
+
+make $*
